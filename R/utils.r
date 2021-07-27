d6b6c055aafe0aa390b518fdca795a99f12d99dc
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-07-27 15:16:06 -0700

    reduce scope of data availability function

--- R/utils.r
+++ R/utils.r
@@ -59,17 +59,17 @@ check_data_availability <- function(ein, year = 2019, source = c("irs", "website
 
 }
 
-#' Check data availability for the combined source
+#' Check data availability for IRS and website
 #'
 #' @param ein An Employment Identification Numbers
 #' @param year A year in which a form was filed. The default value is 2019.
-#' @param sources The data sources. The default value is `c("irs", "website", "twitter", "facebook")`.
+#' @param sources The data sources. The default value is `c("irs", "website")`.
 #' @importFrom purrr map_dfr
 #' @return A datafram that contains three columns: EIN, its data source and availability. Data availability column is a dummy variable. 1 = data exist. 0 = data don't exist.
 #' @export
 #'
 
-check_data_all <- function(ein, year = 2019, sources = c("irs", "website", "twitter", "facebook")) {
+check_data_irs_web <- function(ein, year = 2019, sources = c("irs", "website")) {
 
   out <- map_dfr(sources, ~check_data_availability(ein, year, .))
 
