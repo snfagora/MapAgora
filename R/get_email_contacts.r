d755a0f579d68230975423fc83a381f53a575fa6
Jae Yeon Kim <jaeykim2@gmail.com>
2023-07-24 02:06:16 -0700

    simplify the email extraction process

--- R/get_email_contacts.r
+++ R/get_email_contacts.r
@@ -3,82 +3,82 @@
 #' @param page_url A webpage URL
 #'
 #' @return out A dataframe that contains email contact
-#' @importFrom stringr str_detect
+#' @importFrom stringr str_extract_all
 #' @importFrom httr GET
 #' @importFrom httr content
-#' @importFrom httr content
+#' @importFrom httr set_config
+#' @importFrom httr status_code
+#' @importFrom rvest html_text
 #' @importFrom rvest read_html
 #' @importFrom purrr is_empty
 #' @export
 
 get_email_contact_from_webpage <- function(page_url) {
+  email_pattern <- "\\b[A-Za-z0-9._%+-]+(?:\\[@\\]|\\sat\\s)[A-Za-z0-9.-]+(?:\\[dot\\]|[.])(?:[A-Za-z]{2,}|[A-Za-z]{4,})\\b"
 
-  # Define the three email patterns
-  email_pattern_1 <- "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
-  email_pattern_2 <- "\\b[A-Za-z0-9._%+-]+ at [A-Za-z0-9.-]+ dot [A-Za-z]{2,}\\b"
-  email_pattern_3 <- "\\b[A-Za-z0-9._%+-]+\\[at\\][A-Za-z0-9.-]+\\[dot\\][A-Za-z]{2,}\\b"
+  # Set the timeout to 3 seconds
+  httr::set_config(timeout(3))
 
-  page_content <- content(GET(page_url), type = "text/html; charset=iso-8859-1")
+  # Try to retrieve the page content and handle potential errors
+  tryCatch({
+    # Make the HTTP GET request with the specified timeout
+    response <- GET(page_url, type = "text/html; charset=iso-8859-1")
 
-  if (is_empty(page_content))
-  {
-    out <- data.frame(
-      "page_url" = page_url,
-      "email_contact" = NA
-    )
-  }
+    # Check the status of the response
+    if (status_code(response) == 200) {
+      # Extract the content from the response
+      page_content <- content(response, as = "text")
 
-  webpage <- html_text(page_content)
+      if (is_empty(page_content)) {
+        return(data.frame("page_url" = page_url, "email_contact" = NA))
+      }
 
-  email1 <- regmatches(webpage, gregexpr(email_pattern_1, webpage))
+      webpage <- html_text(page_content)
 
-  email2 <- regmatches(webpage, gregexpr(email_pattern_2, webpage))
+      emails <- unique(str_extract_all(webpage, email_pattern)[[1]])
 
-  email3 <- regmatches(webpage, gregexpr(email_pattern_3, webpage))
+      if (length(emails) == 0) {
+        return(data.frame("page_url" = page_url, "email_contact" = NA))
+      }
 
-  emails <- unlist(c(email1, email2, email3))
+      out <- data.frame("page_url" = page_url, "email_contact" = emails)
 
-  emails <- unique(na.omit(emails))
-
-  emails <- emails[emails != ""]
-
-  if (is_empty(emails))
-  {
-    out <- data.frame(
-      "page_url" = page_url,
-      "email_contact" = NA
-      )
-  } else {
-
-    out <- data.frame(
-      "page_url" = page_url,
-      "email_contact" = emails
-    )
-  }
-
-  return(out)
+      return(out)
+    } else {
+      # If the status code is not 200 (e.g., 404, 500), return NA
+      return(data.frame("page_url" = page_url, "email_contact" = NA))
+    }
+  }, error = function(e) {
+    # Handle any errors (e.g., invalid URLs, connection issues)
+    return(data.frame("page_url" = page_url, "email_contact" = NA))
+  })
 }
+
+
+
 
 #' Extract child links associated with email contacts
 #'
 #' @param base_url A website URL
 #'
 #' @return out A character vector that contains child links associated with email contacts (i.e., page_urls)
-#' @importFrom urltools domain
-#' @importFrom urltools suffix_extract
-#' @importFrom glue glue
 #' @export
 
 get_contact_links_from_website <- function(base_url) {
-
-  # make sure it's a base URL
-  correct_base_url <- suffix_extract(domain(base_url))$host
-
-  contact_url <- glue(correct_base_url, "/contact")
-
-  contacts <- c(correct_base_url, contact_url)
-
-  return(contacts)
-
+  # Check if the base_url starts with "http://" or "https://"
+  if (!grepl("^https?://", base_url, ignore.case = TRUE)) {
+    base_url <- paste0("http://", base_url)
   }
 
+  # Extract the domain from the base_url
+  domain_name <- sub("^https?://", "", base_url)
+  domain_name <- sub("/.*", "", domain_name)
+
+  # Create the contact URL
+  contact_url <- paste0(base_url, "/contact")
+  contact_url <- sub("//contact", "/contact", contact_url)
+
+  # Return the base_url and contact_url
+  contacts <- c(domain_name, contact_url)
+  return(contacts)
+}
