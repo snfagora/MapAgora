72fb41b2fe904d349bf4f0278d67a4e35ff5b876
Jae Yeon Kim <jaeykim2@gmail.com>
2023-07-24 17:29:55 -0700

    caching

--- R/get_email_contacts.r
+++ R/get_email_contacts.r
@@ -4,10 +4,11 @@
 #'
 #' @return out A dataframe that contains email contact
 #' @importFrom stringr str_detect
-#' @importFrom httr GET
+#' @importFrom curl curl_fetch_memory
 #' @importFrom httr content
 #' @importFrom rvest read_html
 #' @importFrom purrr is_empty
+#' @importFrom glue glue
 #' @export
 
 get_email_contact_from_webpage <- function(page_url) {
@@ -17,31 +18,39 @@ get_email_contact_from_webpage <- function(page_url) {
   email_pattern_2 <- "\\b[A-Za-z0-9._%+-]+ at [A-Za-z0-9.-]+ dot [A-Za-z]{2,}\\b"
   email_pattern_3 <- "\\b[A-Za-z0-9._%+-]+\\[at\\][A-Za-z0-9.-]+\\[dot\\][A-Za-z]{2,}\\b"
 
-  page_content <- content(GET(page_url), type = "text/html; charset=iso-8859-1")
+  response <- curl::curl_fetch_memory(page_url)
+
+  # Check if the request was successful
+  if (response$status_code != 200) {
+    out <- data.frame(
+      "page_url" = page_url,
+      "email_contact" = NA
+    )
+    return(out)
+  }
+
+  # Parse the raw content as HTML
+  page_content <- read_html(response$content)
+
+  # Extract the text from the HTML content
+  webpage <- html_text(page_content)
 
   # make sure URL exists
-  if (is.na(page_content)) {
+  if (is_empty(page_content)) {
 
     out <- data.frame(
       "page_url" = page_url,
       "email_contact" = NA
+
     )
 
     return(out)
 
   }
 
-  webpage <- html_text(page_content)
+  emails <- unlist(stringr::str_extract_all(webpage, glue("{email_pattern_1}|{email_pattern_2}|{email_pattern_3}")))
 
-  email1 <- regmatches(webpage, gregexpr(email_pattern_1, webpage))
-
-  email2 <- regmatches(webpage, gregexpr(email_pattern_2, webpage))
-
-  email3 <- regmatches(webpage, gregexpr(email_pattern_3, webpage))
-
-  emails <- unlist(c(email1, email2, email3))
-
-  emails <- unique(na.omit(emails))
+  emails <- tolower(unique(na.omit(emails)))
 
   emails <- emails[emails != ""]
 
@@ -65,25 +74,42 @@ get_email_contact_from_webpage <- function(page_url) {
 #' Extract child links associated with email contacts
 #'
 #' @param base_url A website URL
+#' @importFrom memoise memoise
 #'
 #' @return out A character vector that contains child links associated with email contacts (i.e., page_urls)
 #' @export
 
 get_contact_links_from_website <- function(base_url) {
-  # Check if the base_url starts with "http://" or "https://"
-  if (!grepl("^https?://", base_url, ignore.case = TRUE)) {
-    base_url <- paste0("http://", base_url)
-  }
+
+  get_email_contact_from_webpage <- memoise::memoise(get_email_contact_from_webpage)
 
   # Extract the domain from the base_url
   domain_name <- sub("^https?://", "", base_url)
   domain_name <- sub("/.*", "", domain_name)
 
   # Create the contact URL
-  contact_url <- paste0(base_url, "/contact")
+  contact_url <- paste0(domain_name, "/contact")
   contact_url <- sub("//contact", "/contact", contact_url)
 
   # Return the base_url and contact_url
   contacts <- c(domain_name, contact_url)
   return(contacts)
 }
+
+#' Extract emails from a base url
+#'
+#' @param base_url A website URL
+#'
+#' @return out A dataframe that contains the page URLs and the email addresses appearing on these URLs
+#' @importFrom furrr future_map_dfr
+#' @export
+
+get_emails_from_website <- function(base_url) {
+
+  page_urls <- get_contact_links_from_website(base_url)
+
+  out <- future_map_dfr(page_urls, get_email_contact_from_webpage)
+
+  return(out)
+
+  }
